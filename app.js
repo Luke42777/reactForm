3f091b6263fca172cb2fa0eeecd3c7ac73b3989e
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-24 14:43:08 +0000

    optimization: 1 handleChange method instead 4

--- app.js
+++ app.js
@@ -5,47 +5,51 @@ class Form extends React.Component {
         isLiked: true,
         number: "1",
     }
-    handleCityChange = e => {
-        this.setState({
-            city: e.target.value,
-        })
-    }
 
-    handleCityDescripitionChange = e => {
-        this.setState({
-            description: e.target.value,
-        })
-    }
+    handleChange = e => {
 
-    handleLikeCheck(e){
-        this.setState({
-            isLiked: e.target.checked,
-        })
-    }
+        switch(e.target.name){
+            case "number":
+                this.setState({
+                    number: e.target.value,
+                })
+                break;
+                case "description":
+                    this.setState({
+                        description: e.target.value,
+                    })
+                    break;
+                case "city":
+                        this.setState({
+                            city: e.target.value,
+                        })
+                        break;
+                case "isLiked":
+                            this.setState({
+                                isLiked: e.target.checked,
+                            })
+        }
+    
 
-    handleVisitNumber = (e) =>{
-        this.setState({
-            number: e.target.value,
-        })
     }
 
     render() {
         return (
             <>
                 <label>Provide city:
-                    <input onChange={this.handleCityChange} value={this.state.city} type="text" />
+                    <input name="city" onChange={this.handleChange} value={this.state.city} type="text" />
                 </label>
                 <br />
                 <label>Write something about this town
-                    <br/><textarea onChange={this.handleCityDescripitionChange} value={this.state.description}></textarea>
+                    <br/><textarea name="description" onChange={this.handleChange} value={this.state.description}></textarea>
                 </label>
                 <br />
                 <label>Do you like this town?
-                    <input  type="checkbox" checked={this.state.isLiked} onChange={this.handleLikeCheck}/>
+                    <input name="isLiked"  type="checkbox" checked={this.state.isLiked} onChange={this.handleChange}/>
 
                 </label>
                 <br />
-                <select value={this.state.number} onChange={this.handleVisitNumber.bind(this)}>How many times have you been there?
+                <select name="number" value={this.state.number} onChange={this.handleChange.bind(this)}>How many times have you been there?
                     <option value="0">0</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
